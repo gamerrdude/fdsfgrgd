92a20504be3597183e3ac2f2ae1ec93fd83e26bb
gamerrdude <87709109+gamerrdude@users.noreply.github.com>
2022-01-11 19:29:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//frr raenfv fanevfnm history,jbh,jfbhjbfrhejhaj ghb gmbhrhamhbfjaehr,nha,ehng,jehna,fhearjk,gjrea,ghg,aer,hbfraerkj,haefkjhreaj,h,khfejkh
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+//fwebfruhygfsdkuhweuikugfahedilyhufewiulkureiuywrnehbwifyurge4rhfyiufehyuirlhyaiulgyliurehyafriulyhrfwkuyhwefkyhwakyhdeywkayhywfueadyiuwfydkuy
+function setup(){
+canvas = createCanvas(888, 888);
+canvas.right();
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("doggy", 45, 75);
+    nofill();
+    stroke("#FF0000")
+    ;
+    //hjhsfhghjegshjrjuhw4wiuefhiweuhiuhfewhfjawehhjdfweaUghdyfeahfweahfweyhedyhfehfyfhjfhbjdfk,hhdeuwmhdgwyugwakgwuagdyyjegsgsyafgykfegyug
+    rect(30,60,450,350);
+}
