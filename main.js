75a38f9cfb525c32679350faa2460370b06267a6
gamerrdude <87709109+gamerrdude@users.noreply.github.com>
2022-01-14 19:26:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function preload(){
     img = loadImage('dog_cat.jpg');
+    status = "";
 }
 
 function setup(){
 canvas = createCanvas(640, 420);
 canvas.center();
+objectdetector = ml5.objectdetector('cocossed', modelLoaded);
+document.getElementById("status").innerHTML = "Status: detecting object";
 }
 
 function draw(){
@@ -14,4 +17,25 @@ function draw(){
     nofill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("CAT", 320, 120);
+    nofill()
+    stroke("#FF0000")
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
+    objectdetector.detect(img, gotResult);
+
+}
+
+function gotResult(error, Results){
+if(error){
+    console.log(error);
+
+}
+console.log(results);
 }
